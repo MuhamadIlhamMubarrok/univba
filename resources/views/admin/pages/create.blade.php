d3971777d498e52c1eb974a09ab5c566abc4c668
Abdillah Al Ghifari <abdillahghifari2@gmail.com>
2024-11-13 09:25:14 +0700

    Updated Code

--- resources/views/admin/pages/create.blade.php
+++ resources/views/admin/pages/create.blade.php
@@ -3,33 +3,40 @@
 @section('title', 'Tambah Data Halaman')
 
 @section('content')
-<div class="container">
-    <form method="POST" action="{{ route('pages.store') }}">
-        @csrf
-        <input type="hidden" name="token" value="{{ $token }}">
-        <input type="hidden" name="security" value="{{ md5($token . $secret_key) }}">
+<div class="page-content">
+    <div class="container">
+        <form method="POST" action="{{ route('pages.store') }}">
+            @csrf
+            <input type="hidden" name="token" value="{{ $token }}">
+            <input type="hidden" name="security" value="{{ md5($token . $secret_key) }}">
 
-        <div class="form-group">
-            <label>Judul</label>
-            <input type="text" class="form-control" name="judul" required>
-        </div>
+            <div class="form-group">
+                <label>Judul</label>
+                <input type="text" class="form-control" name="judul" required>
+            </div>
 
-        <div class="form-group">
-            <label>Ringkasan</label>
-            <input type="text" class="form-control" name="short">
-        </div>
+            <div class="form-group">
+                <label>Ringkasan</label>
+                <input type="text" class="form-control" name="short">
+            </div>
 
-        <div class="form-group">
-            <label>Isi Halaman</label>
-            <textarea class="form-control" name="isi" rows="5" required></textarea>
-        </div>
+            <div class="form-group">
+                <label>Isi Halaman</label>
+                <textarea class="form-control" name="isi" id="editor" rows="5" required></textarea>
+            </div>
 
-        <div class="form-group">
-            <label>Link</label>
-            <input type="url" class="form-control" name="link">
-        </div>
+            <div class="form-group">
+                <label>Link</label>
+                <input type="url" class="form-control" name="link">
+            </div>
 
-        <button type="submit" class="btn btn-primary">Simpan</button>
-    </form>
+            <button type="submit" class="btn btn-primary mt-3">Simpan</button>
+        </form>
+    </div>
 </div>
+
+<script src="https://cdn.ckeditor.com/4.21.0/standard/ckeditor.js"></script>
+<script>
+    CKEDITOR.replace('editor');
+</script>
 @endsection
